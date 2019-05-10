70090e2133885a6c6f66dbb24795ca5cc26a2ecd
elliot <elliotgcraig@icloud.com>
2019-05-09 19:23:23 -0600

    just some styling

--- src/Components/General/General.tsx
+++ src/Components/General/General.tsx
@@ -30,83 +30,99 @@ const General = () => {
     return (
         <div className='general'>
             <h2>Calculate Your Mortgage</h2>
-            <h3>Select State</h3>
-            <select value={state} onChange={(e) => updateState(e.target.value)}>
-                {data.state.map((e: string, i: number) => <option key={i}>{e}</option>)}
-            </select>
-            <h3>Select County</h3>
-            <select value={county} onChange={(e) => updateCounty(e.target.value)}>
-                {justCounty.map((e: string, i: number) => <option key={i}>{pascalCase(e)}</option>)}
-            </select>
-            <h3>Select Loan Type</h3>
-            <select value={type} onChange={(e) => updateType(e.target.value)}>
-                {data.type.map((e: string, i: number) => <option key={i}>{e}</option>)}
-            </select>
-            <h3>Select Credit Range</h3>
-            <select value={credit} onChange={(e) => updateCredit(e.target.value)}>
-                {data.credit.map((e: string, i: number) => <option key={i}>{e}</option>)}
-            </select>
-            <h3>Loan Length (years)</h3>
-            <select
-                value={years}
-                onChange={(e) => updateYears(e.target.value)}
-            >
-                {data.years.map((e: number, i: number) => <option key={i}>{e}</option>)}
-            </select>
-            <h3>Loan Rate (default to current rate)</h3>
-            <div>
-                <datalist id="tickmarks">
-                    <option value="2.5" label='2.5%' />
-                    <option value="3" />
-                    <option value="3.5" />
-                    <option value="4" label='4%' />
-                    <option value="4.5" />
-                    <option value="5" />
-                    <option value="5.5" />
-                    <option value="6" label='6%' />
-                </datalist>
-                <input
-                    type="range"
-                    id="start"
-                    name="rate"
-                    list="tickmarks"
-                    min="2.5"
-                    max="6"
-                    step="0.125"
-                    value={rate}
-                    onChange={(e) => updateRate(+e.target.value)}
-                />
-                <label>Rate of {rate}%</label>
-            </div>
-            <p>Are you a Veteran:</p>
+            <div className='input_container'>
+                <div className='state'>
+                    <h3>Select State</h3>
+                    <select value={state} onChange={(e) => updateState(e.target.value)}>
+                        {data.state.map((e: string, i: number) => <option key={i}>{e}</option>)}
+                    </select>
+                </div>
+                <div className='county'>
+                    <h3>Select County</h3>
+                    <select value={county} onChange={(e) => updateCounty(e.target.value)}>
+                        {justCounty.map((e: string, i: number) => <option key={i}>{pascalCase(e)}</option>)}
+                    </select>
+                </div>
+                <div className='loan'>
+                    <h3>Select Loan Type</h3>
+                    <select value={type} onChange={(e) => updateType(e.target.value)}>
+                        {data.type.map((e: string, i: number) => <option key={i}>{e}</option>)}
+                    </select>
+                </div>
+                <div className='credit'>
+                    <h3>Select Credit Range</h3>
+                    <select value={credit} onChange={(e) => updateCredit(e.target.value)}>
+                        {data.credit.map((e: string, i: number) => <option key={i}>{e}</option>)}
+                    </select>
+                </div>
+                <div className='length'>
+                    <h3>Loan Length (years)</h3>
+                    <select
+                        value={years}
+                        onChange={(e) => updateYears(e.target.value)}
+                    >
+                        {data.years.map((e: number, i: number) => <option key={i}>{e}</option>)}
+                    </select>
+                </div>
+                <div className='rate'>
+                    <h3>Loan Rate (default to current rate)</h3>
+                    <div>
+                        <datalist id="tickmarks">
+                            <option value="2.5" label='2.5%' />
+                            <option value="3" />
+                            <option value="3.5" />
+                            <option value="4" label='4%' />
+                            <option value="4.5" />
+                            <option value="5" />
+                            <option value="5.5" />
+                            <option value="6" label='6%' />
+                        </datalist>
+                        <input
+                            type="range"
+                            id="start"
+                            name="rate"
+                            list="tickmarks"
+                            min="2.5"
+                            max="6"
+                            step="0.125"
+                            value={rate}
+                            onChange={(e) => updateRate(+e.target.value)}
+                        />
+                        <label>Rate of {rate}%</label>
+                    </div>
+                </div>
+                <div className='veteran'>
+                    <p>Are you a Veteran:</p>
 
-            <div>
-                <input
-                    type="radio"
-                    id="huey"
-                    name="drone"
-                    value="huey"
-                    onChange={() => updateVeteran(false)}
-                    checked={!veteran}
-                />
-                <label
-                    htmlFor='huey'
-                >No</label>
-            </div>
+                    <div>
+                        <input
+                            type="radio"
+                            id="huey"
+                            name="drone"
+                            value="huey"
+                            onChange={() => updateVeteran(false)}
+                            checked={!veteran}
+                        />
+                        <label
+                            htmlFor='huey'
+                        >No</label>
+                    </div>
 
-            <div>
-                <input
-                    type="radio"
-                    id="dewey"
-                    name="drone"
-                    value="dewey"
-                    onChange={() => updateVeteran(true)}
-                />
-                <label
-                    htmlFor='dewey'
-                >Yes</label>
+                    <div>
+                        <input
+                            type="radio"
+                            id="dewey"
+                            name="drone"
+                            value="dewey"
+                            onChange={() => updateVeteran(true)}
+                        />
+                        <label
+                            htmlFor='dewey'
+                        >Yes</label>
+                    </div>
+                </div>
+                <button>Next</button>
             </div>
-            <button>Next</button>
         </div>
     )
 }
